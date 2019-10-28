eba629e0588a7ce918b7c760da74814e1cc5c858
sgummidipundi <santosh7@stanford.edu>
2019-10-28 08:35:52 -0700

    Added code for reformatting, rounding p-values, Beta estimates, odds ratios, and confidence intervals

--- describe.R
+++ describe.R
@@ -15,3 +15,164 @@ mean_sd <- function(x){
 median_iqr <- function(x){
   paste0(round(median(x), digits = 1)," [",quantile(x, probs = 0.25),", ",quantile(x, probs = 0.75),"]")
 }
+
+#Bolding and rounding for CIs, odds ratios, betas, P-Values
+#------------------------------P-Values-----------------------------#
+cell.spec.p <- function(x, round.val, p.thresh, bold = TRUE) {
+  new_vals <- as.numeric(x)
+  mod_val <- c()
+  for(value in new_vals)
+  {
+    if(is.na(value))
+    {
+      mod_val <- c(mod_val, value)
+    } 
+    if(is.numeric(value) & !is.na(value))
+    {
+      if(value < p.thresh)
+      {
+        if(value < 0.0001)
+        {
+          mod_val <- c(mod_val, paste0("\\textbf{< 0.0001}"))
+        }
+        else if(value >= 0.0001)
+        {
+          mod_val <- c(mod_val, paste0("\\textbf{", round(value, digits = round.val),"}"))
+        }
+      }
+      else if(value >= p.thresh)
+      {
+        mod_val <- c(mod_val, as.character(round(value, digits = round.val)))
+      }
+    }
+  }
+  mod_val
+}
+
+#------------------------------Odds Ratio-----------------------------------#
+cell.spec.or <- function(x, round.val, or.thresh = 1, italic = TRUE) {
+  new_vals <- as.numeric(x)
+  mod_val <- c()
+  for(value in new_vals)
+  {
+    if(is.na(value))
+    {
+      mod_val <- c(mod_val, value)
+    } 
+    if(is.numeric(value) & !is.na(value))
+    {
+      if(value < or.thresh)
+      {
+        if(value < 0.0001)
+        {
+          mod_val <- c(mod_val, paste0("\\em{< 0.0001}"))
+        } else {
+          mod_val <- c(mod_val, paste0("\\em{", round(value, digits = round.val),"}"))
+        }
+      }
+      else if(value >= or.thresh)
+      {
+        if(value > 10000)
+        {
+          mod_val <- c(mod_val, "> 10000")
+        } else {
+          mod_val <- c(mod_val, as.character(round(value, digits = round.val)))
+        }
+      }
+    }
+  }
+  mod_val
+}
+
+
+cell.spec.ci.or <- function(x, round.val)
+{
+  new_vals <- as.numeric(x)
+  mod_val <- c()
+  for(value in new_vals)
+  {
+    if(is.na(value))
+    {
+      mod_val <- c(mod_val, value)
+    }
+    if(is.numeric(value) & !is.na(value))
+    {
+      if(value < 0.001)
+      {
+        mod_val <- c(mod_val, "< 0.001")
+      }
+      else if(value > 100000)
+      {
+        mod_val <- c(mod_val, formatC(value, format = "e", digits = 2))
+      }
+      else
+      {
+        mod_val <- c(mod_val, round(value, digits = round.val))
+      }
+    }
+  }
+  mod_val
+}
+
+
+
+#------------------------------Beta Estimate-----------------------------------#
+cell.spec.beta <- function(x, round.val, beta.thresh = 0, italic = TRUE) {
+  new_vals <- as.numeric(x)
+  mod_val <- c()
+  for(value in new_vals)
+  {
+    if(is.na(value))
+    {
+      mod_val <- c(mod_val, value)
+    } 
+    if(is.numeric(value) & !is.na(value))
+    {
+      if(value < beta.thresh)
+      {
+        if(value < -10000)
+        {
+          mod_val <- c(mod_val, paste0("\\em{< -10000}"))
+        } else {
+          mod_val <- c(mod_val, paste0("\\em{", round(value, digits = round.val),"}"))
+        }
+      }
+      else if(value >= beta.thresh)
+      {
+        if(value > 10000)
+        {
+          mod_val <- c(mod_val, "> 10000")
+        } else {
+          mod_val <- c(mod_val, as.character(round(value, digits = round.val)))
+        }
+        
+      }
+    }
+  }
+  mod_val
+}
+
+cell.spec.ci <- function(x, round.val)
+{
+  new_vals <- as.numeric(x)
+  mod_val <- c()
+  for(value in new_vals)
+  {
+    if(is.na(value))
+    {
+      mod_val <- c(mod_val, value)
+    }
+    if(is.numeric(value) & !is.na(value))
+    {
+      if(value < -100000 & value > 100000)
+      {
+        mod_val <- c(mod_val, formatC(value, format = "e", digits = 2))
+      }
+      else
+      {
+        mod_val <- c(mod_val, round(value, digits = round.val))
+      }
+    }
+  }
+  mod_val
+}
